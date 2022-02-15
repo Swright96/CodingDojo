241218e251c564cd79dc7743904d3f3c8e1fc4ac
swright96 <swright96@live.com>
2022-02-15 07:22:31 -0700

    chaining the code

--- WebFundamentals/python/oop/user.py
+++ WebFundamentals/python/oop/user.py
@@ -1,62 +1,43 @@
 class User :
-    bank_name = "First National Dojo"
-    account_balance = 0
-    
+
     def __init__(self, name, email_address):
         self.name = name
         self.email = email_address
-    
-guido = User("Guido Montoya III", "gmontoya@gmail.com")
+        self.account_balance = 0
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        print(f"Deposit of ${amount} to {self.name} successful!")
+        return self
+
+    def make_widthdrawl(self, amount):
+        self.account_balance -= amount
+        
+        if self.account_balance < amount:
+            print(f"Sorry {self.name}, you have a lack of funds available. Current balance: ${self.account_balance}")
+        else:
+            print(f"Widthdrawl of ${amount} from {self.name} successful!")
+        return self
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: $ {self.account_balance}")
+        return self
+
+    def transfer_money(self, user, amount):
+        self.account_balance -+ amount
+        user.account_balance += amount
+        print(f"{self.name} transfered ${amount} to {user.name}")
+        return self
+
+guido = User("Guido Montoya III", "guidomontoya@gmail.com")
 monty = User("Monty Python", "montythepython@gmail.com")
-james = User("James Charles", "jhcharles@gmail.com")
-    
-
-def __init__(self):
-    self.name = ""
-    self.email = ""
-    self.account_balance = 0
-
-def __init__(other_user):
-    other_user.name = ""
-    other_user.email = ""
-    other_user.account_balance = ""
-
-def make_deposit(self, amount):
-    self.account_balance += amount
-    print(f"Deposit of ${amount} to {self.name} successful!")
-
-def make_widthdrawl(self, amount):
-    self.account_balance -= amount
-    if self.account_balance < amount:
-        print(f"Sorry {self.name}, you have a lack of funds available. Current balance: ${self.account_balance}")
-    else:
-        print(f"Widthdrawl of ${amount} from {self.name} successful!")
-
-def display_user_balance(self):
-    print(f"User: {self.name}, Balance: $ {self.account_balance}")
-
-def transfer_money(self, other_user, amount):
-    self.account_balance -+ amount
-    other_user.account_balance += amount
-    print(f"{self.name} transfered ${amount} to {other_user.name}")
+james = User("James Charles", "jameshcharles@gmail.com")
 
 
-make_deposit(monty, 100)
-make_deposit(monty, 273)
-make_deposit(monty, 543)
-make_widthdrawl(monty, 264)
-display_user_balance(monty)
+monty.make_deposit(100).make_deposit(273).make_deposit(543).make_widthdrawl(264).display_user_balance()
 
-make_deposit(guido, 6000)
-make_deposit(guido, 2500)
-make_widthdrawl(guido, 4000)
-make_widthdrawl(guido, 2000)
-display_user_balance(guido)
+guido.make_deposit(6000).make_deposit(2500).make_widthdrawl(4000).make_widthdrawl(250).display_user_balance()
 
-make_deposit(james, 25)
-make_widthdrawl(james, 7)
-make_widthdrawl(james, 5)
-make_widthdrawl(james, 9)
+james.make_deposit(250).make_widthdrawl(40).make_widthdrawl(80).make_widthdrawl(75)
 
-transfer_money(guido, james, 1000)
-display_user_balance(james)
+guido.transfer_money(james,1000).display_user_balance()
