0cc20531e376959e4dc320febee5b154ae328bda
swright96 <swright96@live.com>
2022-08-15 11:56:58 -0600

    initial commit

---
+++ Projects_Algos/algos/display.js
@@ -0,0 +1,62 @@
+class SLLNode {
+	constructor(val) {
+		this.value = val
+		this.next = null
+	}
+}
+
+class SLL {
+	constructor() {
+		this.head = null
+	}
+
+	addFront(value) {
+		var newNode = new SLLNode(value)
+		newNode.next = this.head
+		this.head = newNode
+		return this.head
+	}
+
+	removeFront() {
+		if (this.head == null) {
+			return this.head
+		}
+		var removeNode = this.head
+		this.head = removeNode.next
+		removeNode.next = null
+		return this.head
+	}
+
+	front() {
+		if (this.head == null) {
+			return null
+		} else {
+			return this.head.value
+		}
+		// return this.head == null ? null : this.head.value
+	}
+
+	display() {
+		var listStr = ""
+
+		if (this.head == null) {
+			return listStr
+		}
+		listStr += this.head.value
+		var runner = this.head.next
+		while (runner != null) {
+			listStr += ", " + runner.value
+		}
+		return listStr
+	}
+}
+
+var mySLL = new SLL()
+mySLL.addFront(10)
+mySLL.addFront(90)
+mySLL.addFront(6)
+mySLL.addFront(12)
+mySLL.removeFront()
+mySLL.removeFront()
+mySLL.removeFront()
+console.log(mySLL.display())
